f73f201cf1c895976e9a1dce47edd848468d33c2
MAG_It_student <tc4evlad@yandex.ru>
2023-11-20 01:16:50 +0300

    fixed trmplate

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,7 @@ function createCard(elemObj, removeCardFunction) {
   const templateNode = cardTemplate.querySelector('.card').cloneNode('true');
   templateNode.querySelector('.card__image').src = elemObj.link;
   templateNode.querySelector('.card__title').textContent = elemObj.name;
-  templateNode.querySelector('.card__title').alt = elemObj.name;
+  templateNode.querySelector('.card__image').alt = elemObj.name;
   templateNode.querySelector('.card__delete-button').addEventListener('click', function(evt){
     removeCardFunction(evt.target);
   });
