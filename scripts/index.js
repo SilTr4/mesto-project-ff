e79fc2ec40483bf210d74633abc2941dc2cf95d0
MAG_It_student <tc4evlad@yandex.ru>
2023-11-20 00:44:08 +0300

    fixed createCard function

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-let index = 0;
 const cardSection = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 const cardAddButton = document.querySelector('.profile__add-button');
@@ -7,21 +6,17 @@ function removeCard(element) {
   element.closest('.card').remove();
 }
 
-function createCard() {
-  if (index === initialCards.length) {
-    index = 0;
-  }
+function createCard(elemObj, removeCardFunction) {
   const templateNode = cardTemplate.querySelector('.card').cloneNode('true');
-  templateNode.querySelector('.card__image').src = initialCards[index].link;
-  templateNode.querySelector('.card__title').textContent = initialCards[index].name;
+  templateNode.querySelector('.card__image').src = elemObj.link;
+  templateNode.querySelector('.card__title').textContent = elemObj.name;
+  templateNode.querySelector('.card__title').alt = elemObj.name;
   templateNode.querySelector('.card__delete-button').addEventListener('click', function(evt){
-    removeCard(evt.target);
+    removeCardFunction(evt.target);
   });
-  cardSection.append(templateNode);
-  index++;
+  return templateNode;
 }
 
-for (let i = 0; i < initialCards.length; i++) {
-  createCard();
-}
-cardAddButton.addEventListener('click', createCard);
+initialCards.forEach(element => {
+  cardSection.append(createCard(element, removeCard));
+});
